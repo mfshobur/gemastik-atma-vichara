71df9544134716d2829f25351dec3279d06a4577
Muh. Shobur Fattah <mfshobur@gmail.com>
2024-05-11 11:13:19 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/atma_vichara_gemastik/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atma_vichara_gemastik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/enum.dart
@@ -0,0 +1,5 @@
+enum ReflectionType {
+  past,
+  present,
+  future,
+}

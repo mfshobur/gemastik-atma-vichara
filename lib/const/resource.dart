4037feedd23ae7ca21939884c5f0c40c6e23a1de
Muh. Shobur Fattah <mfshobur@gmail.com>
2024-05-11 12:14:15 +0800

    chore: add assets

---
+++ lib/const/resource.dart
@@ -0,0 +1,9 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  /// ![preview](file:///Users/mfshobur/Documents/flutter/atma_vichara_gemastik/assets/icons/icon.png)
+  static const String ASSETS_ICONS_ICON_PNG = 'assets/icons/icon.png';
+}
